39b8fe67f86170f652c6e982c2d4254f8fc014cc
gax-il <lukas.pav.2002@gmail.com>
2024-07-29 15:43:03 +0200

    Fix

--- src/components/layout/MobileButtons.tsx
+++ src/components/layout/MobileButtons.tsx
@@ -7,17 +7,25 @@ import { Button } from "../ui/button";
 
 const MobileButtons = () => {
   const rawPathname = usePathname();
-  const pathname = rawPathname.replace("/md", "");
 
   return (
     <>
       {NavItems.map((NavItem, index) => (
         <div key={index}>
-          <Link href={NavItem.href}>{NavItem.label}</Link>
+          <Link href={NavItem.md ? `/md${NavItem.href}` : `${NavItem.href}`}>
+            {NavItem.label}
+          </Link>
           {NavItem.children && (
             <div className="flex flex-col ml-4">
               {NavItem.children.map((child, childIndex) => (
-                <Link key={childIndex} href={`${NavItem.href}/${child.href}`}>
+                <Link
+                  key={childIndex}
+                  href={
+                    NavItem.md
+                      ? `/md${child.href ? child.href : NavItem.href}`
+                      : `${NavItem.href}/${child.href}`
+                  }
+                >
                   {child.label}
                 </Link>
               ))}
